838db99d33a5520acb238d32224442025e212b75
JohnyKovalenko1337 <13371488zeus@gmail.com>
2020-10-15 11:57:16 +0300

    making code looking better

--- src/util/fetchinNotify.ts
+++ src/util/fetchinNotify.ts
@@ -1,5 +1,5 @@
 import Fetching from '../util/fetch';
-
+const fetch = require('node-fetch');
 const main = async (user: any, link: string) => {
 
 
@@ -7,28 +7,25 @@ const main = async (user: any, link: string) => {
 
     const result: any = await Fetching('GET', link);
     const data = await result.json();
-    console.log(data);
 
-    console.log(user.email, user.name);
     if (data.fees.length > 0) {
         data.fees.forEach(async (fee: any) => {
             console.log(fee.date)
             let date = new Date(fee.date);
-            console.log(new Date().getTime());
-            console.log(date.getTime());
-            console.log(new Date().getTime() - date.getTime());
-            const ms = 300000000 // 5 min in miliseconds
+            const ms = 300000 // 5 min in miliseconds
+
             if (new Date().getTime() - date.getTime() < ms) {
                 console.log('should to be send')
-                await Fetching('POST', `${serverUrl}/api/email/new-fee/toUser/`, JSON.stringify({
-                    email: user.email,
-                    name: user.name,
-                    link: 'google.com'
-                }))
-                await Fetching('POST', `${serverUrl}/api/push/new-fee/toUser/`, JSON.stringify({
+                await Fetching('POST', `${serverUrl}api/email/new-fee/toUser/`,
+                    JSON.stringify({
+                        email: user.email,
+                        name: user.name,
+                        link: 'google.com'
+                    })
+                )
+                await Fetching('POST', `${serverUrl}api/push/new-fee/toUser/`, JSON.stringify({
                     registrationToken: 'user.registrationToken'
                 }));
-
             }
             else {
                 console.log("no need to send");
